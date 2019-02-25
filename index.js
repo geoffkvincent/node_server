7237342514ac9df8faeeb751220d7670887eb990
Geoff Vincent <geoffkvincent@gmail.com>
2019-02-25 14:15:49 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/test', (req, res) => {
+  res.send({ fuck: 'you'})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
