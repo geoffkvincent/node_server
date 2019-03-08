0f270372f0274b3597ffa481eeacbf94b3e01706
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-08 14:26:33 -0700

    server setup

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     }, 
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then( (existingUser) => {
-          if (existingUser) {
-            //already have user with that id
-            done(null, existingUser)
-          } else {
-            //we dont have one
-            new User({ googleId: profile.id })
-              .save()
-              .then( user => done(null, user) )
-          }
-        })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+      if (existingUser) {
+      //already have user with that id
+      done(null, existingUser)
+      } else {
+        //we dont have one
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user)
+      }
     }
   )
 )
