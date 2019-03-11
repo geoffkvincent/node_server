5c704c122a4666532de8b57c3d283c954c879acf
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-11 12:13:40 -0600

    actions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import  React, {Fragment}  from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
 import Landing from './Landing'
 import Dashboard from './Dashboard'
 import SurveyForm from './SurveyForm'
@@ -9,11 +10,13 @@ const App = () => {
   return (
     <Fragment>
       <Navbar />
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route exact path='/surveys' component={Dashboard} />
-        <Route exact path='/survey/new' component={SurveyForm} />
-      </Switch>
+      <Container>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/surveys' component={Dashboard} />
+          <Route exact path='/survey/new' component={SurveyForm} />
+        </Switch>
+      </Container>
     </Fragment>
   )
 }
