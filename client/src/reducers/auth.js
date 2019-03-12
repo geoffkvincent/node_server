2bf69bc1e65f2687edfceb467eb8450ad68cd00a
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-12 13:48:31 -0600

    redux actions and types for auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,12 @@
-export default function(state={}, action) {
+import axios from 'axios'
+
+const FETCH_USER = 'FETCH_USER'
+
+export const fetchUser = () => {
+  
+}
+
+export default function(state = {}, action) {
   switch(action.type) {
     default:
       return state
