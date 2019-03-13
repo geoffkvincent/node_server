51600c858bfcc253f919efb519d92aa2dd1e5489
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-13 12:52:23 -0600

    auth setup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,7 +16,11 @@ class Navbar extends React.Component {
           </NavLink>
         )
       default:
-        return 'Logout'
+          return (
+            <NavLink to='/api/logout'>
+              Logout
+            </NavLink>
+          )
     }
   }
 
