899c6b58838e19b902225fcff2150bba1abc0a15
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-15 14:38:07 -0600

    body parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 const keys = require('./config/keys')
+const bodyParser = require('body-parser')
 require('./models/user')
 require('./services/passport')
 
@@ -10,13 +11,13 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 )
-
 app.use(passport.initialize())
 app.use(passport.session())
 
