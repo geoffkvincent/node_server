5c56140a2a609055161b7ee9bc335c114287bcb1
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-18 12:11:46 -0600

    heroku bug fix

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-if (process.env.NODE_ENV === 'produciton') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   const path = require('path')
   app.get('*', (req, res) => {
