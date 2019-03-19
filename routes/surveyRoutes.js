091876a21af2cc133b578a55076baeec34d3d908
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-19 14:30:18 -0600

    survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,5 +7,12 @@ const Survey = mongoose.model('surveys')
 module.exports = (app) => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email }))
+    })
   })
 }
