ddabddcf0c4b121ef19313f6d9a8a0de7d25c3de
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-21 15:58:16 -0600

    dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button, Icon, Container, Header } from 'semantic-ui-react'
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <div>Dashboard</div>
+      <Container>
+        <Header></Header>Dashboard
+        <Icon name='plus square outline' size='huge'/>
+      </Container>
     )
   }
 }
