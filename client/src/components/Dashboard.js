fad46257efbaeb1652223ebb7ebf2a44bfcd749d
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-21 16:02:00 -0600

    dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Icon, Container, Header } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 class Dashboard extends React.Component {
   render() {
     return (
       <Container>
         <Header as='h1'>Dashboard</Header>
-        <Icon name='plus square outline' size='huge'/>
+        <Link to='/api/surveys'>
+          <Icon name='plus square outline' size='huge'/>
+        </Link>
       </Container>
     )
   }
