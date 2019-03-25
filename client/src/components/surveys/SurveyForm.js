d89198affdc3cdcce2c442636aab028ea82af75a
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-25 10:09:34 -0600

    survey form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,10 +4,19 @@ import { Form } from 'semantic-ui-react'
 import SurveyField from './SurveyField'
 
 class SurveyForm extends React.Component {
+
+  renderFields() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
   render() {
     return (
       <Form>
-        <Field type="text" name="surveyTitle" component="input" />
+        {this.renderFields}
         <Form.Button>Submit</Form.Button>
         <SurveyField />
       </Form>
